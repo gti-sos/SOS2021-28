d004d7f61d11eed5ea34561f6b49f5c4825ceb9c
spartano27 <spartano.jm7@gmail.com>
2021-03-13 11:07:02 +0000

    Punto 2 del trabajo en grupo del backlog

---
+++ index.js
@@ -0,0 +1,17 @@
+var cool = require("cool-ascii-faces");
+var express = require("express");
+var app = express();
+var port = 10000;
+
+app.get("/cool",(request,response) => {
+	
+	response.send(cool());
+	console.log("New Request to /cool has arrived");
+	
+});
+
+app.listen(port,() => {  
+	
+	console.log("Server ready listening on port " + port );
+	
+});
