7efbdf35538da8a01d432219987290b8989cf6a1
MarinaBC <marina_manoxa@hotmail.com>
2021-04-29 16:48:57 +0000

    Cambios en Home.svelte

---
+++ src/Pages/Home.svelte
@@ -0,0 +1,167 @@
+<script>
+  import {
+    Button,
+    Icon,
+    Card,
+    CardBody,
+    CardFooter,
+    CardHeader,
+    CardSubtitle,
+    CardText,
+    CardTitle,
+    Col,
+    Container,
+    Row,
+  } from "sveltestrap";
+</script>
+
+<main>
+  <h1>SOS2021-28</h1>
+
+  <h2>Team</h2>
+  <Container>
+    <Col>
+      <a href="https://github.com/spartano27">José Manuel Martín de los Santos</a>
+    </Col>
+    <Col>
+      <a href="https://github.com/MarinaBC">Marina Barragán Candel</a>
+    </Col>
+    <Col>
+      <a href="https://github.com/Fernasilver">Fernando Olmedo Delgado</a>
+    </Col>
+  </Container>
+  <p />
+  <h2>Project description</h2>
+  <p>
+    Investigación y Analisis de las ventas de videojuegos y su relacion con la cantidad de consolas vendidas
+	y los premios que haya obtenido dicho videojuego.
+  </p>
+
+  <Container>
+    <Col>
+      <a href="http://github.com/gti-sos/SOS2021-28"
+        ><b>Repository: </b>http://github.com/gti-sos/SOS2021-01</a
+      >
+    </Col>
+    <Col>
+      <a href="http://sos2021-01.herokuapp.com"
+        ><b>URL: </b>http://sos2021-01.herokuapp.com</a
+      >
+    </Col>
+  </Container>
+  <p />
+
+  <a href="/#/info">
+    <Button color="primary">Info</Button>
+  </a>
+
+  <p />
+
+  <h2>APIs</h2>
+  <Container>
+    <Col xs="auto">
+      <Card class="mb-3">
+        <CardHeader>
+          <CardTitle><h5>Awards</h5></CardTitle>
+        </CardHeader>
+        <CardBody>
+          <CardSubtitle />
+          
+          <a href="https://sos2021-01.herokuapp.com/api/v1/awards">
+            <Button color="primary">Backend</Button></a
+          >
+          <a href="/#/awards">
+            <Button color="primary">Frontend</Button>
+          </a>
+          <a href="https://documenter.getpostman.com/view/14941757/TzJoE12q"
+            ><Button color="info">Postman Documentation</Button></a
+          >
+        </CardBody>
+        <CardFooter
+          >developed by
+          <a href="https://github.com/spartano27">José Manuel Martín de los Santos</a>
+		  </CardFooter>
+      </Card>
+    </Col>
+	
+	
+	
+    <Col xs="auto">
+      <Card class="mb-3">
+        <CardHeader>
+          <CardTitle><h5>Platforms</h5></CardTitle>
+        </CardHeader>
+        <CardBody>
+          <CardSubtitle />
+          
+          <a href="https://sos2021-01.herokuapp.com/api/v1/platforms">
+            <Button color="primary">Backend</Button>
+          </a>
+          <a href="/#/platforms">
+            <Button color="primary">Frontend</Button>
+          </a>
+          <a href="hhttps://documenter.getpostman.com/view/14944973/TzJsexb1">
+            <Button color="info">Postman Documentation</Button>
+          </a>
+        </CardBody>
+        <CardFooter
+          >developed by
+          <a href="https://github.com/MarinaBC">Marina Barragán Candel</a>
+        </CardFooter>
+      </Card>
+    </Col>
+	
+	
+	
+	
+	
+	
+    <Col xs="auto">
+      <Card class="mb-3">
+        <CardHeader>
+          <CardTitle><h5>Games</h5></CardTitle>
+        </CardHeader>
+        <CardBody>
+          <CardSubtitle />
+          <a href="/api/v1/games"
+            ><Button color="primary">Backend</Button></a
+          >
+          <a href="/#/games"
+            ><Button color="primary">Frontend</Button></a
+          >
+          <a href="https://documenter.getpostman.com/view/14967482/TzJu8cmV"
+            ><Button color="info">Postman Documentation</Button></a
+          >
+        </CardBody>
+        <CardFooter
+          >developed by
+          <a href="https://github.com/Fernasilver">Fernando Olmedo Delgado</a
+          ></CardFooter
+        >
+      </Card>
+    </Col>
+  </Container>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+  b {
+    color: #00c1ff;
+  }
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
--- src/Pages/NotFound.svelte
+++ src/Pages/NotFound.svelte
@@ -1,3 +1,34 @@
+<script>
+import {
+    Button
+  } from "sveltestrap";
+</script>
+
 <main>
-    <h1>¡Esta página no existe!</h1>
+  <h2>404</h2>
+<h4>Página no encontrada</h4>
+<p>La página que estabas buscando no existe. Tal vez hallas escrito 
+  mal la dirección o esta ha cambiado.</p>
+  <a href="/">
+    <Button color="primary">Volver a la página principal</Button></a>
+
 </main>
+
+<style>
+    main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+    max-width: 380px;
+  }
+  h2{
+    font-size: 100px;
+  }
+  h4{
+    background-color: grey;
+    color: white;
+    padding: 10px;
+    margin: 5px;
+    
+  }
+</style>
