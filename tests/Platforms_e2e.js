e0585cc866aeb7af5d7e75783db29d69559d8ea8
Marina <marina_manoxa@hotmail.com>
2021-05-27 11:22:52 +0200

    nuevo test mejorado

--- tests/Platforms_e2e.js
+++ tests/Platforms_e2e.js
@@ -23,62 +23,54 @@ const puppeteer = require('puppeteer');
     console.log("Timeout! Taking an screenshot...");
     await page.screenshot({ path: './tests/screenshots/platforms/02.png' });//Foto de tabla de plataformas
     
-    /////////
-    await page.click("body > main > main > ul > li:nth-child(3) > a");
 
     await page.waitForTimeout(2000);
     var initialrowCount = (await page.$$("body > main > main > table:nth-child(7) > tbody > tr")).length;
-
+    await page.waitForTimeout(2000);
 
 
     console.log(`Initial row count = ${initialrowCount}`);
     if(initialrowCount != 9){
         console.error("Initial row count is not 8")
-        //process.exit(1)
+        process.exit(1)
     }
-
+    await page.waitForTimeout(2000);
     
-    /*
+    
     await page.$eval('body > main > main > table:nth-child(7) > tbody > tr:nth-child(1) > td:nth-child(1) > input[type=text]', el => el.value = 'PlatformPrueba');
     await page.$eval('body > main > main > table:nth-child(7) > tbody > tr:nth-child(1) > td:nth-child(2) > input[type=text]', el => el.value = 'PlatformPrueba');
     await page.$eval('body > main > main > table:nth-child(7) > tbody > tr:nth-child(1) > td:nth-child(3) > input[type=number]', el => el.value = '2021');
     await page.$eval('body > main > main > table:nth-child(7) > tbody > tr:nth-child(1) > td:nth-child(4) > input[type=number]', el => el.value = '199999');
     await page.$eval('body > main > main > table:nth-child(7) > tbody > tr:nth-child(1) > td:nth-child(5) > input[type=number]', el => el.value = '8');
    
-
-    await page.screenshot({ path: './tests/screenshots/platforms/---03.png' });
-    await page.$eval('#add', el => el.click());
-    */
-   
     await page.screenshot({ path: './tests/screenshots/platforms/03.png' });
-    
-
-
+    await page.$eval('body > main > main > table:nth-child(7) > tbody > tr:nth-child(1) > td:nth-child(6) > button', el => el.click()); //Insertar dato
     await page.waitForTimeout(2000);
-    var pushrowCount = (await page.$$("body > main > main > table:nth-child(7) > tbody > tr:nth-child(1)")).length;
-
-    await page.screenshot({ path: './tests/screenshots/platforms/04.png' });
-
-    console.log(`Push row count = ${pushrowCount}`);
-    if(initialrowCount + 1  != pushrowCount){
-        //console.error("bad push")
-        //process.exit(1)
-    }
-/*
-    await page.waitForTimeout(3000);  
     
-    await page.click("body > main > main > nav > ul > li.active.page-item > a > span:nth-child(1)");
+    await page.click("body > main > main > ul > li:nth-child(4) > a");
+    await page.waitForTimeout(3000);
+    await page.screenshot({ path: './tests/screenshots/platforms/04.png' }); //borrar todo
 
-    await page.screenshot({ path: './tests/screenshots/platforms/05.png' });
-    await page.waitForTimeout(2000);
-    */
-    
+    await page.waitForTimeout(3000);
+
+    //Cancelar
+    await page.click("body > main > main > ul > li:nth-child(4) > div > div.modal.show.d-block > div > div > div.modal-footer > button.btn.btn-secondary");
+    //
+    await page.waitForTimeout(3000);
+    await page.click("body > main > main > ul > li:nth-child(3) > a");
+    await page.waitForTimeout(3000);
+    await page.screenshot({ path: './tests/screenshots/platforms/05.png' }); //cargar todo
+    await page.waitForTimeout(3000);
+    //Cancelar
+    await page.click("body > main > main > ul > li:nth-child(3) > div > div.modal.show.d-block > div > div > div.modal-footer > button.btn.btn-secondary");
+    //
+
     var finalrowCount = (await page.$$("body > main > main > table:nth-child(7) > tbody > tr:nth-child(1)")).length;
 
     console.log(`final row count = ${finalrowCount}`);
-    if(1 != finalrowCount){
-        //console.error("not reset")
-        //process.exit(1)
+    if(0 != finalrowCount){
+        console.error("not reset")
+        process.exit(1)
     }
     await browser.close();
 
